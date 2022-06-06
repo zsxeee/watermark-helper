5cd09f3370b9ff320c337593050113a4a086fcd5
zsxeee <sixu.zhou@basefount.com>
2022-06-06 10:15:31 +0800

    perf: 下载文件名将从源文件生成，而不是浏览器默认生成；完善README

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const minify = require('html-minifier').minify;
 const fs = require("fs");
 const Base64 = require('js-base64').Base64;
 
+const file = fs.readFileSync('./watermark-helper.html', 'utf-8')
+let minified = minify(file, {
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    minifyJS: true,
+    removeAttributeQuotes: true,
+    sortAttributes: true
+});
+
+fs.writeFileSync('./watermark-helper.min.html', minified, 'utf-8');
+
+const dataLink = 'data:text/html;base64,' + Base64.encode(minified);
+
+fs.writeFileSync('./data-link.txt', dataLink);
+
+const readme = fs.readFileSync('./README.md', 'utf-8');
+
+fs.writeFileSync('./README.md', readme.replace(/\[此链接]\([^)]*\)/g, `[此链接](${dataLink})`), 'utf-8')
+
 fs.readFile('./watermark-helper.html', 'utf-8', (e, data) => {
     if (e) throw Error(`Read failed: ${e.message}`);
 
